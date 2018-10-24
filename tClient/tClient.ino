73e3675ab75c778c1a79fcb2d6b41b1f61ebfaea
Oscar Reid <osctec@hotmail.co.uk>
2018-10-24 21:09:14 +0100

    Added ThingSpeak support
    The devices now communicate through ThingSpeak

--- tClient/tClient.ino
+++ tClient/tClient.ino
@@ -1,73 +1,110 @@
-//working v1.2
-#include <ESP8266WiFi.h>
+//working v1.3
+#include <ESP8266WiFi.h>                           //Adds library for WiFi
+#include <ThingSpeak.h>                            //Adds library for cloud service
 
-const int inputPin = 2;
-int pirState = LOW;
+WiFiClient client;                                 //Initialize WiFi Client
+const int inputPin = 2;                            //
+int pirState = LOW;                                //Set default sensor state
 int val = 0;
 
-const char *ssid = "IoT";
-const char *password = "p@ssword321";
+//const char *ssid = "IoT";
+//const char *password = "p@ssword321";
+
+const char *ssid = "M.I.6 SURVEILLANCE VAN 2";     //WiFi Name
+const char *password = "JR38D0TdMLef6ml4";         //WiFi Password
+
+// ThingSpeak information
+char thingSpeakAddress[] = "api.thingspeak.com";   //Cloud service address
+unsigned long channelID = 607501;                  //Channel ID
+char* readAPIKey = "BVQ2L7CXBAOLN8II";             //Key to read from channel
+char* writeAPIKey = "GE8UZRGHVTQJV2VO";            //Key to write to channel
+const unsigned long postingInterval = 10L * 1000L; //Left is how many seconds as right is
+unsigned int dataFieldOne = 1;                     //Field to write temperature data
+unsigned int aField = 6;                           //Field to hold first constant of the thermistor calibration
+
+int sendValue = 1;
 
 void setup() {
-  pinMode(inputPin, INPUT);
-  Serial.begin(115200);
-  delay(10);
+  pinMode(inputPin, INPUT);                        //Sets Sensor input pin
+  Serial.begin(115200);                            //Sets serial baud
+  delay(10);                                       //
 
-  // Explicitly set the ESP8266 to be a WiFi-client
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  WiFi.mode(WIFI_STA);                             // Explicitly set the ESP8266 to be a WiFi-client
+  WiFi.begin(ssid, password);                      //Starts connection to WiFi using given values
+  Serial.print("Connecting");                      //Lets user know it's connecting
+  while (WiFi.status() != WL_CONNECTED) {          //While waiting to connect
+    Serial.print(".");                             //print a dot every
+    delay(500);                                    //500 ms
   }
+  Serial.print("Connected");                       //Once connected print connected
+  ThingSpeak.begin(client);                        //Connect to ThingSpeak
 
 }
 
 void loop() {
-  int sendValue = 1;
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const char * host = "192.168.4.1";
-  const int httpPort = 80;
-
-  //const char * host2 = "192.168.4.1";//ip of relay
-  //const int httpPort2 = 85;//port of relay
-
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-  // We now create a URI for the request. E.G. /data/?showerSensor=20
-  String url = "/data/";
-  url += "?showerSensor=";
-  url += sendValue;
-
-  //New code
+  //int sendValue = 1;
   val = digitalRead(inputPin);
   if (val == HIGH) {
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    delay(150);
-//    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-//                 "Host: " + host2 + "\r\n" +
-//                 "Connection: close\r\n\r\n");
-  }
-
-  //End of new code
-
-  // This will send the request to the server
-
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
+    if (sendValue == 1) {
+      writeTSData(channelID, dataFieldOne, sendValue);
+      sendValue = 0;
+    } else {
+      writeTSData(channelID, dataFieldOne, sendValue);
+      sendValue = 1;
     }
+    
+    delay(500);
   }
 
-  delay(500);
+
+
+
+
+  // Use WiFiClient class to create TCP connections
+
+  //  const char *host = "192.168.4.1";
+  //  const int httpPort = 80;
+  //
+  //  if (!client.connect(host, httpPort)) {
+  //    Serial.println("connection failed");
+  //    return;
+  //  }
+
+  // We now create a URI for the request. E.G. /data/?showerSensor=20
+  //  String url = "/data/";
+  //  url += "?showerSensor=";
+  //  url += sendValue;
+  //
+  //  val = digitalRead(inputPin);
+  //  if (val == HIGH) {
+  //    //write2TSData(channelID, dataFieldOne, "1");
+  //    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  //                 "Host: " + host + "\r\n" +
+  //                 "Connection: close\r\n\r\n");
+  //    delay(150);
+  //  }
+  //  unsigned long timeout = millis();
+  //  while (client.available() == 0) {
+  //    if (millis() - timeout > 5000) {
+  //      Serial.println(">>> Client Timeout !");
+  //      client.stop();
+  //      return;
+  //    }
+  //  }
+  //  delay(500);
+
+}
+
+
+
+
+int writeTSData( long TSChannel, unsigned int TSField, float data ) {
+  int  writeSuccess = ThingSpeak.writeField( TSChannel, TSField, data, writeAPIKey ); // Write the data to the channel
+  if ( writeSuccess ) {
+
+    Serial.println( String(data) + " written to Thingspeak." );
+  }
+
+  return writeSuccess;
 }
--- tServer/tServer.ino
+++ tServer/tServer.ino
@@ -1,31 +1,41 @@
-//working v1.2
+//working v1.3
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
 #include <Servo.h>
+#include <ThingSpeak.h>
 
+WiFiClient client;
 const int buttonPin = 2;
 int buttonState = 0;
 int angle = 0;
+boolean btnPressed = false;
 
-const char *ssid = "IoT";
-const char *password = "p@ssword321";
+//const char *ssid = "IoT";
+//const char *password = "p@ssword321";
 
-ESP8266WebServer server(80);
+
+const char *ssid = "M.I.6 SURVEILLANCE VAN 2";     //WiFi Name
+const char *password = "JR38D0TdMLef6ml4";         //WiFi Password
+
+// ThingSpeak information
+char thingSpeakAddress[] = "api.thingspeak.com";   //Cloud service address
+unsigned long channelID = 607501;                  //Channel ID
+char* readAPIKey = "BVQ2L7CXBAOLN8II";             //Key to read from channel
+char* writeAPIKey = "GE8UZRGHVTQJV2VO";            //Key to write to channel
+long lastReadUpdateTime = 0;
+long lastWriteUpdateTime = 0;
+const unsigned long readingInterval = 30L * 1000L; //Time between reads
+const unsigned long postingInterval = 30L * 1000L; //Time between writes
+unsigned int dataFieldOne = 1;                     //Field to write occupied data
+unsigned int dataFieldTwo = 2;                     //Field to write angle data
+unsigned int aField = 6;                           //Field to hold first constant of the thermistor calibration
+
+
+
+//ESP8266WebServer server(80);
 Servo servo;
 
-void handleSentVar() {
-  if (server.hasArg("showerSensor")) { // this is the variable sent from the client
-    int readingInt = server.arg("showerSensor").toInt();
-    Serial.println("Recieved");
-    Serial.println(readingInt);
-    //Code that doesn't work
-    if (readingInt == 1) {
-      angle = 160;
-    }
-    //End of code that doesn't work
-    server.send(200, "text/html", "Data received");
-  }
-}
+
 
 void setup() {
   pinMode(buttonPin, INPUT);
@@ -33,20 +43,43 @@ void setup() {
   Serial.begin(115200);
   delay(10000);
 
-  WiFi.softAP(ssid, password);
-  IPAddress myIP = WiFi.softAPIP();
 
-  server.on("/data/", HTTP_GET, handleSentVar); // when the server receives a request with /data/ in the string then run the handleSentVar function
-  server.begin();
-  Serial.println(myIP);
-  Serial.print("Setup done");
+  WiFi.mode(WIFI_STA);                             // Explicitly set the ESP8266 to be a WiFi-client
+  WiFi.begin(ssid, password);                      //Starts connection to WiFi using given values
+  Serial.print("Connecting");                      //Lets user know it's connecting
+  while (WiFi.status() != WL_CONNECTED) {          //While waiting to connect
+    Serial.print(".");                             //print a dot every
+    delay(500);                                    //500 ms
+  }
+  Serial.print("Connected");                       //Once connected print connected
+  ThingSpeak.begin(client);                        //Connect to ThingSpeak
+
+  //  WiFi.softAP(ssid, password);
+  //  IPAddress myIP = WiFi.softAPIP();
+  //
+  //  server.on("/data/", HTTP_GET, handleSentVar); // when the server receives a request with /data/ in the string then run the handleSentVar function
+  //  server.begin();
+  //  Serial.println(myIP);
+  //  Serial.print("Setup done");
+
+
 
 }
 
 void loop() {
-  server.handleClient();
+  //server.handleClient();
+  //  if ((int)readTSData(channelID, dataFieldOne) == 1) {
+  //    angle = 160;
+  //  }
+  //angle = (int)readTSData(channelID, dataFieldTwo);
+  //writeTSData(channelID, dataFieldOne, sendValue);
+  //  if (millis() - lastReadUpdateTime >=  readingInterval) {
+  //    lastReadUpdateTime = millis();
+  //
+  //  }
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
+    btnPressed = true;
     //servo.attach(0);
     angle = angle + 5;
     delay(50);
@@ -54,23 +87,53 @@ void loop() {
       delay(200);
       angle = 0;
     }
+
   } else {
     //servo.detach();
   }
-//  if (correctPosition()) {
-//    servo.detach()
-//  } else {
-//    servo.attach(0);
-//  }
+  if (millis() - lastWriteUpdateTime >=  postingInterval) {
+    lastWriteUpdateTime = millis();
+    if (btnPressed) {
+      writeTSData(channelID, dataFieldTwo, angle);
+      btnPressed = false;
+    }
+    angle = (int) readTSData(channelID, dataFieldTwo);
+  }
+
   servo.write(angle);
 }
 
-//boolean correctPosition() {
-//  servo.attach(0);
-//  if (servo.read() == angle) {
-//    return true;
-//  } else {
-//    return false;
+
+
+float readTSData(long TSChannel, unsigned int TSField ) {
+
+  float data =  ThingSpeak.readFloatField( TSChannel, TSField, readAPIKey );
+  Serial.println( " Data read from ThingSpeak: " + String( data, 9 ) );
+  return data;
+
+}
+
+int writeTSData( long TSChannel, unsigned int TSField, float data ) {
+  int  writeSuccess = ThingSpeak.writeField( TSChannel, TSField, data, writeAPIKey ); // Write the data to the channel
+  if ( writeSuccess ) {
+
+    Serial.println( String(data) + " written to Thingspeak." );
+  }
+
+  return writeSuccess;
+}
+
+
+//void handleSentVar() {
+//  if (server.hasArg("showerSensor")) { // this is the variable sent from the client
+//    int readingInt = server.arg("showerSensor").toInt();
+//    Serial.println("Recieved");
+//    Serial.println(readingInt);
+//    //Code that doesn't work
+//    if (readingInt == 1) {
+//      angle = 160;
+//    }
+//    //End of code that doesn't work
+//    server.send(200, "text/html", "Data received");
 //  }
-//  servo.detach();
 //}
